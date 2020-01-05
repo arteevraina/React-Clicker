9d4ebf4cbb2bac815e7b4121ea06358604b68f9e
Your Name <arteevraina@gmail.com>
2020-01-05 22:47:03 +0530

    Hooks

---
+++ src/ClickerusingHooks.js
@@ -0,0 +1,35 @@
+import React,{useState} from "react"
+import "./Clicker.css"
+
+const Clicker = () => {
+    //Declare a new state variable which we call count
+
+    const[count,setCount] = useState(0)
+
+    function handleInc() {
+        setCount(count + 1)
+    }
+
+    function handleRes() {
+        setCount(0)
+    }
+
+    function handleDec() {
+        setCount(count - 1)
+    }
+
+        return(
+        <div className="clicker">        
+            <div className="display">
+                <b>{count}</b>
+            </div>
+            <div className="btn">
+                <button onClick={handleInc} className="btn-btn1"><b>+</b></button>
+                <button onClick={handleRes} className="btn-btn2"><b>RESET</b></button>
+                <button onClick={handleDec} className="btn-btn3"><b>-</b></button>
+            </div>
+        </div>
+        )
+}
+
+export default Clicker
