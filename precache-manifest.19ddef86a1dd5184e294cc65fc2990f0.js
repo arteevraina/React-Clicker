9c8f62844ec29182dbe1ff44dc00956284f41dd6
Your Name <arteevraina@gmail.com>
2020-02-23 03:13:59 +0530

    Updates

---
+++ precache-manifest.19ddef86a1dd5184e294cc65fc2990f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d35400720f71b94e66fbeb59134de5e",
+    "url": "/React-Clicker/index.html"
+  },
+  {
+    "revision": "0cb8dc4277a4efe4ff9e",
+    "url": "/React-Clicker/static/css/main.09efa03c.chunk.css"
+  },
+  {
+    "revision": "aee01ccc2b28e947c809",
+    "url": "/React-Clicker/static/js/2.6539fda5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Clicker/static/js/2.6539fda5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cb8dc4277a4efe4ff9e",
+    "url": "/React-Clicker/static/js/main.cea0c609.chunk.js"
+  },
+  {
+    "revision": "1d87acaaa3fef5f751a6",
+    "url": "/React-Clicker/static/js/runtime-main.607e3c8b.js"
+  }
+]);
